4b2ad0f65bc183dc9256504d5d399594d7224fbb
kkampardi <kkampardi@gmail.com>
2016-06-10 10:33:27 +0300

    Use LoginRequiredMixin and PermissionRequiredMixin form third party django-braces module to limit access to our views #5

--- courses/views.py
+++ courses/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.core.urlsolvers import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from braces.views import LoginRequieredMixin, PermissionRequiredMixin
 
 from .models import Course
 
@@ -22,8 +23,10 @@ class OwnerEditMixin(object):
         return super(OwnerEditMixin, self).form_valid(form)
 
 
-class OwnerCourseMixin(object):
-    model = courses
+class OwnerCourseMixin(OwnerMixin, LoginRequieredMixin):
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
 
 
 class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
@@ -38,14 +41,15 @@ class ManageCourseListView(OwnerCourseMixin, ListView):
     template_name = 'courses/manage/course/list.html'
 
 
-class CourseCreateView(OwnerCourseEditMixin, CreateView):
-    pass
+class CourseCreateView(PermissionRequiredMixin, OwnerCourseEditMixin, CreateView):
+    permission_required = 'courses.add_course'
 
 
-class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
-    pass
+class CourseUpdateView(PermissionRequiredMixin, OwnerCourseEditMixin, UpdateView):
+    permission_required = 'courses.change_course'
 
 
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
     success_url = reverse_lazy('manage_course_list')
+    permission_required = 'courses.delete_course'
