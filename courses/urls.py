5c629408687c415a900ffdfa96146fdea0654fa0
Katerina Kampardi <kkampardi@gmail.com>
2016-09-08 11:00:34 +0300

    Display all modules for a course and list contents for a specific module #5

--- courses/urls.py
+++ courses/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
         name='module_content_update'),
     url(r'^content/(?P<id>\d+)/delete/$', views.ContentDeleteView.as_view(),
         name='module_content_delete'),
+    url(r'^module/(?P<module_id>\d+)/$', views.ModuleContentListView.as_view(),
+        name='module_content_list'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -174,3 +174,21 @@ class ContentDeleteView(View):
         content.delete()
 
         return redirect('module_content_list', module.id)
+
+
+"""
+    Display all Modules for a course and list contents for a specific
+    module
+"""
+
+class ModuleContentListView(TemplateResponseMixin, View):
+    template_name = "courses/manage/module/content_list.html"
+
+    def get(self, request, module_id):
+        module = get_object_or_404(Module,
+                                   id=module_id,
+                                   course__owner = request.user )
+
+        return self.render_to_response({
+            'module': module,
+        })
