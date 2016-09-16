0691f1cd939c800986d74cea0975434bf5d7564e
Katerina Kampardi <kkampardi@gmail.com>
2016-09-16 17:12:21 +0300

    Create a form for students to enroll courses #11

---
+++ students/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from courses.models import Course
+
+
+# create students enroll form
+class CourseEnrollForm(forms.Form):
+    course = forms.ModelChoiceField(queryset=Course.objects.all(),
+                                    widget=forms.HiddenInput)
