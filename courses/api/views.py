5a25f3aac37123d058f5231bbac04e749dd7f10a
Katerina Kampardi <kkampardi@gmail.com>
2016-11-09 15:05:56 +0200

    Create viewsets and routers #19
    - Add course viewse
    - Register api URL resovlers

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -6,7 +6,7 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from ..models import Subject, Course
-from .serializers import SubjectSerializer
+from .serializers import SubjectSerializer, CourseSerializer
 
 
 class SubjectListView(generics.ListAPIView):
@@ -26,3 +26,7 @@ class CourseEnrollView(APIView):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
