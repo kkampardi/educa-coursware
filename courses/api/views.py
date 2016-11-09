bfb3ea678b5cef30e9beba43b5cf1d11eb296fe1
Katerina Kampardi <kkampardi@gmail.com>
2016-11-09 14:29:52 +0200

    Handle authentication when course enroll #19

--- courses/api/views.py
+++ courses/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
 
 from ..models import Subject, Course
 from .serializers import SubjectSerializer
@@ -17,6 +18,8 @@ class SubjectDetailView(generics.RetrieveAPIView):
 
 
 class CourseEnrollView(APIView):
+    authentication_classes = (BasicAuthentication,)
+    
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
