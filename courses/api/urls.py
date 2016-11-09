2b72eb49e569dd3aea1c3c7763feb1496a35b011
Katerina Kampardi <kkampardi@gmail.com>
2016-11-09 12:20:22 +0200

    Retrieve subject objects #19
    - Create list and detail views
    - Define api url resolvers
    - Include the api url resolvers into generla project urls

---
+++ courses/api/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from . import views
+
+
+urlpatterns = [
+        url(r'^subjects/$',views.SubjectListView.as_view(),
+            name='subject_list'),
+
+        url(r'^subjects/(?P<pk>\d+)/$',views.SubjectDetailView.as_view(),
+            name='subject_detail'),
+]
