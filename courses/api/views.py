93765b236c667981d21df89c41517d23fa0abad2
Katerina Kampardi <kkampardi@gmail.com>
2016-11-09 14:46:21 +0200

    Add permitions when enrolling course #19

--- courses/api/views.py
+++ courses/api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from ..models import Subject, Course
 from .serializers import SubjectSerializer
@@ -19,7 +20,8 @@ class SubjectDetailView(generics.RetrieveAPIView):
 
 class CourseEnrollView(APIView):
     authentication_classes = (BasicAuthentication,)
-    
+    permission_classes = (IsAuthenticated,)
+
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
