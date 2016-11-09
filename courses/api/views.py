44529a7058f23103fa10c6aa360d695e12dfb0ee
Katerina Kampardi <kkampardi@gmail.com>
2016-11-09 12:58:01 +0200

    Add nested serializers for courses #19

--- courses/api/views.py
+++ courses/api/views.py
@@ -7,7 +7,6 @@ class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
-
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
